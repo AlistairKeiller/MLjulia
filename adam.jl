3846508a13b04d816cc0aec86b49d8456bd6257c
Alistair Keiller <alistair@keiller.net>
2023-07-10 12:13:21 -0700

    add batching to adam

--- adam.jl
+++ adam.jl
@@ -18,7 +18,7 @@ function error(model::Model, in::Vector{Float32}, out::Vector{Float32})
     sum((in - out) .^ 2)
 end
 
-function train(model::Model, train_data, learning_rate, β1=0.9, β2=0.999, ϵ=1e-8)
+function train(model::Model, train_data, learning_rate=1e-3, batch_size=256, β1=0.9, β2=0.999, ϵ=1e-8)
     # Initialize first and second moment estimates for weights and biases
     m_w = [zeros(size(w)) for w in model.weights]
     v_w = [zeros(size(w)) for w in model.weights]
@@ -29,37 +29,40 @@ function train(model::Model, train_data, learning_rate, β1=0.9, β2=0.999, ϵ=1
     while true
         t += 1
         println("error: ", sum(error(model, in, out) for (in, out) ∈ test_data) / length(test_data))
-        for (x, y) in train_data
-            # Forward pass
-            pre_activations = []
-            activations = [x]
-            for (w, b, a) in zip(model.weights, model.biases, model.activations)
-                push!(pre_activations, w * activations[end] + b)
-                push!(activations, a(pre_activations[end]))
-            end
 
-            # Backward pass
-            δ = 2 * (activations[end] - y) .* model.activations′[end](pre_activations[end])
-            for l in length(model.weights):-1:1
-                ∇b = δ
-                ∇w = δ * activations[l]'
-                if l != 1
-                    δ = (model.weights[l]' * δ) .* model.activations′[l](pre_activations[l-1])
+        for batch in [train_data[i:min(i+batch_size-1, end)] for i in 1:batch_size:length(train_data)]
+            for (x, y) in batch
+                # Forward pass
+                pre_activations = []
+                activations = [x]
+                for (w, b, a) in zip(model.weights, model.biases, model.activations)
+                    push!(pre_activations, w * activations[end] + b)
+                    push!(activations, a(pre_activations[end]))
                 end
 
-                # Update weights and biases
-                m_w[l] = β1 * m_w[l] + (1 - β1) * ∇w
-                v_w[l] = β2 * v_w[l] + (1 - β2) * ∇w .^ 2
-                m_hat_w = m_w[l] / (1 - β1^t)
-                v_hat_w = v_w[l] / (1 - β2^t)
+                # Backward pass
+                δ = 2 * (activations[end] - y) .* model.activations′[end](pre_activations[end])
+                for l in length(model.weights):-1:1
+                    ∇b = δ
+                    ∇w = δ * activations[l]'
+                    if l != 1
+                        δ = (model.weights[l]' * δ) .* model.activations′[l](pre_activations[l-1])
+                    end
 
-                m_b[l] = β1 * m_b[l] + (1 - β1) * ∇b
-                v_b[l] = β2 * v_b[l] + (1 - β2) * ∇b .^ 2
-                m_hat_b = m_b[l] / (1 - β1^t)
-                v_hat_b = v_b[l] / (1 - β2^t)
+                    # Update weights
+                    m_w[l] = β1 * m_w[l] + (1 - β1) * ∇w
+                    v_w[l] = β2 * v_w[l] + (1 - β2) * ∇w .^ 2
+                    m_hat_w = m_w[l] / (1 - β1^t)
+                    v_hat_w = v_w[l] / (1 - β2^t)
+                    model.weights[l] -= learning_rate .* m_hat_w ./ (sqrt.(v_hat_w) .+ ϵ)
 
-                model.weights[l] -= learning_rate * m_hat_w ./ (sqrt.(v_hat_w) .+ ϵ)
-                model.biases[l] -= learning_rate * m_hat_b ./ (sqrt.(v_hat_b) .+ ϵ)
+                    # Update biases
+                    m_b[l] = β1 * m_b[l] + (1 - β1) * ∇b
+                    v_b[l] = β2 * v_b[l] + (1 - β2) * ∇b .^ 2
+                    m_hat_b = m_b[l] / (1 - β1^t)
+                    v_hat_b = v_b[l] / (1 - β2^t)
+                    model.biases[l] -= learning_rate .* m_hat_b ./ (sqrt.(v_hat_b) .+ ϵ)
+                end
             end
         end
     end
@@ -77,6 +80,5 @@ layers::Vector{Tuple{Int,Int,Function,Function}} = [(28 * 28, 128, relu, relu′
 model = Model(layers)
 train_data = [(vec(in), [Float32(i == out) for i ∈ 0:9]) for (in, out) ∈ MNIST(:train)]
 test_data = [(vec(in), [Float32(i == out) for i ∈ 0:9]) for (in, out) ∈ MNIST(:test)]
-learning_rate = Float32(1e-3)
 
 train(model, train_data, learning_rate)
